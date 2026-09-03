d5d78b02178c4404226365360665b305b1cd522e
janer2 <377017270@qq.com>
2026-09-03 16:34:40 +0800

    First commit

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 # xsmallappliance
 catalogue
-=======
-# xsmallappliance
-catalogue
->>>>>>> 86d8de835b32ca0a7865b4c71f3742ef8894092c
--- start-server.bat
+++ start-server.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 chcp 65001 >nul
 title XSmallAppliance Server
@@ -17,23 +16,3 @@ if %errorlevel%==0 (
     "C:\Users\Administrator\AppData\Roaming\Accio\pre-install\ab1f8a6ee51b\python\python.exe" -m http.server 8080
 )
 pause
-=======
-@echo off
-chcp 65001 >nul
-title XSmallAppliance Server
-echo.
-echo   XSmallAppliance - Catalog Viewer
-echo   http://localhost:8080
-echo   Press Ctrl+C to stop
-echo.
-start http://localhost:8080
-cd /d "%~dp0"
-
-where python >nul 2>nul
-if %errorlevel%==0 (
-    python -m http.server 8080
-) else (
-    "C:\Users\Administrator\AppData\Roaming\Accio\pre-install\ab1f8a6ee51b\python\python.exe" -m http.server 8080
-)
-pause
->>>>>>> 86d8de835b32ca0a7865b4c71f3742ef8894092c
